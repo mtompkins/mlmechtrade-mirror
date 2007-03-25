d0d6a603fed6d0a9ef6f04da07ebf8db314feab3
pranas_baliuka <pranas_baliuka@355ed7ff-352a-0410-a45a-cab21d7ce132>
2007-03-25 10:26:00 +0000

    Not usable implementations. Standard gzip from JDK used instead

--- utils/compression/quicklz/quicklz.h
+++
@@ -1,6 +0,0 @@
-unsigned int qlz_compress(const void *source, char *destination, unsigned int size);
-unsigned int qlz_decompress(const char *source, void *destination);
-unsigned int qlz_size_decompressed(const char *source);
-unsigned int qlz_size_compressed(const char *source);
-unsigned int qlz_compress_packet(const void *source, char *destination, unsigned int size, char *buffer);
-unsigned int qlz_decompress_packet(const char *source, void *destination, char *buffer);
