00dfaa83d8ed0fb7c3c66fcc0a7333b13ba8a31c
pranas_baliuka <pranas_baliuka@355ed7ff-352a-0410-a45a-cab21d7ce132>
2007-04-16 20:08:46 +0000

    performance improvement for exportTimeSeries2cvs.m

--- strategyst/M/exportTimeSeries2cvs.m
+++ strategyst/M/exportTimeSeries2cvs.m
@@ -2,7 +2,7 @@ function exportTimeSeries2cvs(data)
 for i = 1:size(data.marketData,2)
     symbol = data.marketData(i);
     fileName = strcat(symbol.symbol,'.CSV');
-    data2export = [str2num(DATESTR(symbol.time,'yyyymmdd')) symbol.data];
+    data2export = [str2num(datestr(symbol.time,'yyyymmdd')) symbol.data];
     fid = fopen(fileName,'wt');
     fprintf(fid,'%8.0f,%0.4f,%0.4f,%0.4f,%0.4f,%0.0f\n', data2export');
     fclose(fid);
