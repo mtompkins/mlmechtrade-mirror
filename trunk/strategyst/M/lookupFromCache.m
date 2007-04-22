6375d2a97e2544ad24a6d29052dc2266925f9f05
pranas_baliuka <pranas_baliuka@355ed7ff-352a-0410-a45a-cab21d7ce132>
2007-04-22 16:42:55 +0000

    Last decompressed data chunk is cashed as global var TS_CASHE_MANAGER for intra day data. Better performance comparing to WeakHashMap!

--- trunk/strategyst/M/lookupFromCache.m
+++ trunk/strategyst/M/lookupFromCache.m
@@ -1,4 +1,9 @@
 function data = lookupFromCache(key)
 casheManager = evalin('base', 'TS_CASHE_MANAGER');
-data = casheManager.get(key);
+%data = casheManager.get(key);
+if (casheManager.key == key)
+    data = casheManager.value;
+else
+    data = [];
+end
 
