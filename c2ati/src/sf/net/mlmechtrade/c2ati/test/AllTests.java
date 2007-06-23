e5d98612bcfc024f9d3fffc755b4599f44f85024
pranas_baliuka <pranas_baliuka@355ed7ff-352a-0410-a45a-cab21d7ce132>
2007-06-23 16:24:05 +0000

    C2ATI: Unit Testing

--- c2ati/src/sf/net/mlmechtrade/c2ati/test/AllTests.java
+++ c2ati/src/sf/net/mlmechtrade/c2ati/test/AllTests.java
@@ -9,10 +9,13 @@ public class AllTests {
 		TestSuite suite = new TestSuite(
 				"Test for sf.net.mlmechtrade.c2ati.test");
 		// $JUnit-BEGIN$
-		suite.addTestSuite(C2ATIErrorTest.class);
-		suite.addTestSuite(C2ATITest.class);
 		suite.addTestSuite(c2ATIACKTest.class);
+		suite.addTestSuite(C2ATIErrorTest.class);
+		suite.addTestSuite(C2ATIGetAllSignalsTest.class);
+		suite.addTestSuite(C2ATIRequestSystemListTest.class);
 		suite.addTestSuite(C2ATISyncTest.class);
+		suite.addTestSuite(C2ATITest.class);
+		suite.addTestSuite(TestUtilTest.class);
 		// $JUnit-END$
 		return suite;
 	}
--- c2ati/src/sf/net/mlmechtrade/c2ati/test/C2ATISyncTest.java
+++ c2ati/src/sf/net/mlmechtrade/c2ati/test/C2ATISyncTest.java
@@ -11,6 +11,8 @@ import org.xml.sax.SAXException;
 
 import sf.net.mlmechtrade.c2ati.C2ATI;
 import sf.net.mlmechtrade.c2ati.C2ATIError;
+import sf.net.mlmechtrade.c2ati.domain.AssetEnum;
+import sf.net.mlmechtrade.c2ati.domain.C2Position;
 import sf.net.mlmechtrade.c2ati.domain.C2SystemState;
 import junit.framework.TestCase;
 
@@ -26,5 +28,34 @@ public class C2ATISyncTest extends TestCase {
 			ParserConfigurationException, SAXException, C2ATIError {
 		List<C2SystemState> systemSync = fixture.requestSystemSync();
 		assertNotNull(systemSync);
+		assertEquals(2, systemSync.size());
+		// 1
+		C2SystemState systemState = systemSync.get(0);
+		assertEquals(158523, systemState.getSystemId());
+		assertEquals("YYY", systemState.getSystemName());
+		assertEquals(1181841762, systemState.getTimeFilterSecs());
+		assertEquals("2007-06-14 13:22:42", systemState.getTimeFilterClock());
+		List<C2Position> positions = systemState.getPositions();
+		assertEquals(2, positions.size());
+		C2Position position = positions.get(0);
+		assertEquals("CCCC", position.getSymbol());
+		assertEquals(AssetEnum.stock, position.getAssetType());
+		assertEquals(false, position.isFund());
+		assertEquals(132, position.getQuant());
+		assertEquals("", position.getUnderlying());
+		assertEquals("", position.getRight());
+		assertEquals("0", position.getStrike());
+		assertEquals("", position.getExpir());
+		assertEquals("", position.getExchange());
+		assertEquals("1", position.getMarketCode());
+		position = positions.get(1);
+		assertEquals("YHOO", position.getSymbol());
+		// 2
+		systemState = systemSync.get(1);
+		assertEquals("GGGG", systemState.getSystemName());
+		positions =  systemState.getPositions();
+		assertEquals(1, positions.size());
+		position = positions.get(0);
+		assertEquals(true, position.isFund());
 	}
 }
