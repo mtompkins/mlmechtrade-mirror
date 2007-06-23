70d50622d2190dac8348bed225374d79561263aa
virginija <virginija@355ed7ff-352a-0410-a45a-cab21d7ce132>
2007-06-23 10:08:00 +0000

    C2ATI: Unit Testing

--- trunk/c2ati/src/sf/net/mlmechtrade/c2ati/test/c2ATISyncTest.java
+++ trunk/c2ati/src/sf/net/mlmechtrade/c2ati/test/c2ATISyncTest.java
@@ -56,6 +56,11 @@ public class c2ATISyncTest extends TestCase {
 		String actual2 = fixture.multFillConfirmCommandString(fillConfirmList, MultFillConfirmEnum.mult3fillconfirm);
 		assertEquals(expected2, actual2);
 		fixture.multFillConfirm(fillConfirmList, MultFillConfirmEnum.mult3fillconfirm);
+		fixture.multFillConfirm(fillConfirmList, MultFillConfirmEnum.multfillconfirmcumu);
+		String expected3 ="http://64.68.145.33:7878?cmd=multfillconfirmcumu&session=267127353762716967&h=host&filldata=122|sep|20|sep|aaaa|sep|12.05|sep|443|sep|35|sep|bbb|sep|23.16|&live=0";
+		String actual3 = fixture.multFillConfirmCommandString(fillConfirmList, MultFillConfirmEnum.multfillconfirmcumu);
+		assertEquals(expected3, actual3);
+		fixture.multFillConfirm(fillConfirmList, MultFillConfirmEnum.multfillconfirmcumu);
 		fixture.logOff();
 	}
 }
