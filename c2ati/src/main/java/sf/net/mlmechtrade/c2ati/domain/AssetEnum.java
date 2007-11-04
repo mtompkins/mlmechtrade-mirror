415917027d320ae219898446c46f87d7a2d3e0c2
pranas_baliuka <pranas_baliuka@355ed7ff-352a-0410-a45a-cab21d7ce132>
2007-11-04 08:41:40 +0000

    Moved to SF algotrade project

--- c2ati/src/main/java/sf/net/mlmechtrade/c2ati/domain/AssetEnum.java
+++
@@ -1,5 +0,0 @@
-package sf.net.mlmechtrade.c2ati.domain;
-
-public enum AssetEnum {
-	stock, futures, options, forex
-}
--- c2ati/src/main/java/sf/net/mlmechtrade/metadata/Parameters.java
+++
@@ -1,9 +0,0 @@
-package sf.net.mlmechtrade.metadata;
-
-import java.util.HashMap;
-
-public class Parameters <_Param extends Enum> extends HashMap<_Param, Parameter<_Param>>{
-
-	private static final long serialVersionUID = 1L;
-
-}
