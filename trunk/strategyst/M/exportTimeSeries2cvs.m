91b8d55d1b375f15945de6e66a1f26f5eb99a228
pranas_baliuka <pranas_baliuka@355ed7ff-352a-0410-a45a-cab21d7ce132>
2008-05-20 11:31:34 +0000

    Additional parameter added for exportTimeSeries2cvs.m

--- trunk/strategyst/M/exportTimeSeries2cvs.m
+++ trunk/strategyst/M/exportTimeSeries2cvs.m
@@ -1,11 +1,22 @@
-function exportTimeSeries2cvs(data)
+function exportTimeSeries2cvs(data, dateTimeFormat)
 % Exports data to CSV files 
-% yyyymmddHHMM data format,
+% yyyymmddHHMM data format, or specified via parameter dateTimeFormat
+% Note: default version is more efficient
 for i = 1:size(data.marketData,2)
     symbol = data.marketData(i);
     fileName = strcat(symbol.symbol,'.CSV');
-    data2export = [str2num(datestr(symbol.time,'yyyymmddHHMM')) symbol.data];
     fid = fopen(fileName,'wt');
-    fprintf(fid,'%12.0f,%0.4f,%0.4f,%0.4f,%0.4f,%0.0f\n', data2export');
+    if (nargin == 1)
+        data2export = [str2num(datestr(symbol.time,'yyyymmddHHMM')) symbol.data];
+        fprintf(fid,'%12.0f,%0.4f,%0.4f,%0.4f,%0.4f,%0.0f\n', data2export');
+    else
+        if (nargin == 2)
+            for j = 1:size(data.marketData.data,1)
+                fprintf(fid, '%s,', datestr(symbol.time(j),dateTimeFormat));
+                fprintf(fid,'%0.4f,%0.4f,%0.4f,%0.4f,%0.0f', symbol.data(j,:));
+                fprintf(fid,'\n');
+            end % for j
+        end 
+    end
     fclose(fid);
-end
+end % for i
