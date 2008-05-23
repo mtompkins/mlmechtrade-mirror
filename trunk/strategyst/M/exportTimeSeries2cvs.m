e8eb0f1ac3df1a127e416dcd0139f8ef211f7ab3
pranas_baliuka <pranas_baliuka@355ed7ff-352a-0410-a45a-cab21d7ce132>
2008-05-23 23:34:57 +0000

    bug fix

--- trunk/strategyst/M/exportTimeSeries2cvs.m
+++ trunk/strategyst/M/exportTimeSeries2cvs.m
@@ -11,7 +11,7 @@ for i = 1:size(data.marketData,2)
         fprintf(fid,'%12.0f,%0.4f,%0.4f,%0.4f,%0.4f,%0.0f\n', data2export');
     else
         if (nargin == 2)
-            for j = 1:size(data.marketData.data,1)
+            for j = 1:size(data.marketData(i).data,1)
                 fprintf(fid, '%s,', datestr(symbol.time(j),dateTimeFormat));
                 fprintf(fid,'%0.4f,%0.4f,%0.4f,%0.4f,%0.0f', symbol.data(j,:));
                 fprintf(fid,'\n');
